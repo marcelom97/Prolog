8bba3b879b1548b491bf97c4e0558866f9496b95
marcelom97 <marcelomollaj@gmail.com>
2020-01-08 17:21:30 +0200

    [added] extended ex2

--- project5/ex2.pl
+++ project5/ex2.pl
@@ -6,35 +6,28 @@ enqueue(X, [], [X]).
 queue_element_cardinality(Q1, Q2) :-
     temp_fun(Q1, Q2).
 
-
 temp_fun(Q1, Q2) :-
     empty_queue(Q2),
-    empty_queue(Queue),
-    write_head_tail(Q1, Q2, Queue).
-    % write(Queue),
-    % nl,
-    % write(Q2).
+    write(Q2),
+    write_head_tail(Q1, Q2).
 
-write_head_tail([], _, _).
-write_head_tail([H|T], Q2, Queue) :-
+write_head_tail([], _).
+write_head_tail([H|T], Q2) :-
     compound_name_arguments(H, _, B),
-    write_splitted(B, Q2, Queue),
-    write_head_tail(T, Q2, Queue).
+    write_splitted(B, Q2),
+    write_head_tail(T, Q2).
 
-write_splitted([], [], _).
-write_splitted([H|T], Q2, Queue) :-
-    push_to_queue(H, T, Q2, Queue).
+write_splitted([], _).
+write_splitted([H|T], Q2) :-
+    push_to_queue(H, T, Q2).
 
-push_to_queue(_, [0], Q2, Queue) :-
+push_to_queue(_, [0], Q2) :-
     nl,
-    write('Q2:'),write(Q2),nl,
-    write('Queue:'),write(Queue),
+    write('Q2:'),
+    write(Q2),
     !.
-        % append(Queue, Q2).
-push_to_queue(H, [T|_], Q2, Queue) :-
-    % write(H),
-    append(Queue, [H], NewQueue),
-    % write(NewQueue),
-    % nl,
+
+push_to_queue(H, [T|_], Q2) :-
+    append(Q2, [H], NewQueue),
     NewT is T-1,
-    push_to_queue(H, [NewT], Q2, NewQueue).
+    push_to_queue(H, [NewT], NewQueue).
